6997f27a4c2bac08689882915d2b2a068e031ffc
Nelman25 <jonelvillaver735@gmail.com>
2024-09-06 14:14:02 +0800

    unicafe step 3 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,32 +1,42 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
+
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
+    return (
+        <div>
+            <h1>statistics</h1>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+            <p>all {total}</p>
+            <p>average {average}</p>
+            <p>positive {positive}%</p>
+        </div>
+    );
+};
 
 const App = () => {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
-    const [total, setTotal] = useState(0);
+    // const [total, setTotal] = useState(0);
 
+    const total = good + bad + neutral;
     const goodScore = 1 * good;
     const badScore = -1 * bad;
     const average = total ? (goodScore + badScore) / total : 0;
     const positive = total ? (good / total) * 100 : 0;
 
     const handleGoodFeedback = () => {
-        const updatedGood = good + 1;
-        setGood(updatedGood);
-        setTotal(updatedGood + neutral + bad);
+        setGood(good + 1);
     };
 
     const handleNeutralFeedback = () => {
-        const updatedNeutral = neutral + 1;
-        setNeutral(updatedNeutral);
-        setTotal(good + updatedNeutral + bad);
+        setNeutral(neutral + 1);
     };
 
     const handleBadFeedback = () => {
-        const updateBad = bad + 1;
-        setBad(updateBad);
-        setTotal(good + neutral + updateBad);
+        setBad(bad + 1);
     };
 
     return (
@@ -37,15 +47,14 @@ const App = () => {
                 <button onClick={handleNeutralFeedback}>neutral</button>
                 <button onClick={handleBadFeedback}>bad</button>
             </div>
-            <div>
-                <h1>statistics</h1>
-                <p>good {good}</p>
-                <p>neutral {neutral}</p>
-                <p>bad {bad}</p>
-                <p>all {total}</p>
-                <p>average {average}</p>
-                <p>positive {positive}%</p>
-            </div>
+            <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={total}
+                average={average}
+                positive={positive}
+            />
         </div>
     );
 };
