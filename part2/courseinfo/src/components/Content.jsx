7ed68a452ace621f8a514009261ffeac38e3cfd9
Nelman25 <jonelvillaver735@gmail.com>
2024-09-07 09:36:03 +0800

    Course information step 7 done

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -2,11 +2,21 @@
 import Part from "./Part";
 
 const Content = ({ parts }) => {
+    const exercises = parts.map((part) => part.exercises);
+    const total = exercises.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+    );
     return (
         <div>
             {parts.map((part) => (
-                <Part key={part.id} name={part.name} exercise={part.exercises} />
+                <Part
+                    key={part.id}
+                    name={part.name}
+                    exercise={part.exercises}
+                />
             ))}
+            <h3>total of {total} exercises</h3>
         </div>
     );
 };
