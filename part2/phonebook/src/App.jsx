4db5fbbd575c8060ead08c2dfbb03feeea9c9831
Nelman25 <jonelvillaver735@gmail.com>
2024-09-12 14:48:27 +0800

    2.9: The phonebook Step 4 done, added search feature to filter the names in contacts

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,15 +4,28 @@ import { useState } from "react";
 const App = () => {
     const [persons, setPersons] = useState([
         { name: "Arto Hellas", number: "040-123456", id: 1 },
+        { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+        { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+        { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
     ]);
     const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("");
+    const [searchText, setSearchText] = useState("");
 
     const names = persons.map((person) => person.name.toLowerCase());
     const nameAlreadyExist = names.includes(newName.toLowerCase());
+    const namesToRender = persons.filter((person) => {
+        const personName = person.name.toLowerCase();
+        return personName.includes(searchText.toLowerCase());
+    });
 
     const addPerson = (e) => {
         e.preventDefault();
+
+        if (newName === "" || newNumber === "") {
+            console.log("There's a missing input, try again");
+            return;
+        }
 
         if (nameAlreadyExist) {
             alert(`${newName} is already added to the phonebook`);
@@ -40,9 +53,22 @@ const App = () => {
         setNewNumber(e.target.value);
     };
 
+    const handleChangeSearch = (e) => {
+        setSearchText(e.target.value);
+    };
+
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                filter shown with
+                <input
+                    type="text"
+                    value={searchText}
+                    onChange={handleChangeSearch}
+                />
+            </div>
+            <h2>Add a new</h2>
             <form onSubmit={addPerson}>
                 <div>
                     name:
@@ -57,7 +83,7 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map((person) => (
+            {namesToRender.map((person) => (
                 <li key={person.id}>
                     {person.name} {person.number}
                 </li>
