855b6eb7adcd187b471c0731ae0e6dea52b677e3
Nelman25 <jonelvillaver735@gmail.com>
2024-09-28 16:27:41 +0800

    2.20* Data for countries step 3 done!!!

--- part2/Countries/src/components/CountryInfo.jsx
+++ part2/Countries/src/components/CountryInfo.jsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 const CountryInfo = ({ countryName }) => {
 	const [countryData, setCountryData] = useState({});
 	const [countryLocation, setCountryLocation] = useState({});
+	const [countryWeather, setCountryWeather] = useState({});
 
 	useEffect(() => {
 		const api_key = "7d3ea97ed27567aff251356392514703";
@@ -15,41 +16,47 @@ const CountryInfo = ({ countryName }) => {
 				`https://studies.cs.helsinki.fi/restcountries/api/name/${countryName}`
 			)
 			.then((response) => {
-				const data = {
+				const importantCountryData = {
 					name: response.data.name.common,
 					capital: response.data.capital,
 					area: response.data.area,
 					languages: response.data.languages,
 					flagImg: response.data.flags.png,
 				};
-				setCountryData(data);
+				setCountryData(importantCountryData);
 				axios
 					.get(
-						`http://api.openweathermap.org/geo/1.0/direct?q=${data.capital}&limit=1&appid=${api_key}`
+						`http://api.openweathermap.org/geo/1.0/direct?q=${importantCountryData.capital}&limit=1&appid=${api_key}`
 					)
 					.then((response) => {
-						response.data.map((data) => {
-							const location = {
-								lat: data.lat,
-								lon: data.lon,
-							};
-							setCountryLocation(location);
-							axios
-								.get(
-									`https://api.openweathermap.org/data/2.5/weather?lat=${data.lat.toFixed(
-										2
-									)}&lon=${data.lon.toFixed(2)}&appid=${api_key}`
-								)
-								.then((response) => {
-									const data = Object.values(response.data);
-									data.map((data) => {
-										console.log(data);
-										console.log(data.weather);
-									});
+						const data = response.data;
+						const location = {
+							lat: data[0].lat,
+							lon: data[0].lon,
+						};
+						setCountryLocation(location);
+
+						axios
+							.get(
+								`https://api.openweathermap.org/data/2.5/weather?lat=${location.lat.toFixed(
+									2
+								)}&lon=${location.lon.toFixed(2)}&appid=${api_key}`
+							)
+							.then((response) => {
+								const fetchWeatherIcon = response.data.weather[0].icon;
+								const wind = `wind ${response.data.wind.speed}m/s`;
+								const temperature = `${(
+									response.data.main.temp - 273.15
+								).toFixed(2)}°C`;
+								const weatherIcon = `https://openweathermap.org/img/wn/${fetchWeatherIcon}@2x.png`;
+
+								setCountryWeather({
+									temp: temperature,
+									icon: weatherIcon,
+									wind: wind,
 								});
-						});
-					})
-					.catch((error) => console.error(error));
+							});
+					});
 			})
 			.catch((error) => console.error("there was an error ", error));
 	}, [countryName]);
@@ -73,7 +80,9 @@ const CountryInfo = ({ countryName }) => {
 						alt="country flag"
 					/>
 					<h2>Weather in {countryData.capital}</h2>
-					<p></p>
+					<p>Temperature: {countryWeather.temp}</p>
+					<img src={countryWeather.icon} />
+					<p>{countryWeather.wind}</p>
 				</>
 			) : (
 				<></>
