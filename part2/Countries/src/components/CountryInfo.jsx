f68fb32b6c1757c0050504a052b7b81214dbf047
Nelman25 <jonelvillaver735@gmail.com>
2024-09-28 12:18:53 +0800

    lat and long can now be fetched

--- part2/Countries/src/components/CountryInfo.jsx
+++ part2/Countries/src/components/CountryInfo.jsx
@@ -1,11 +1,16 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 const CountryInfo = ({ countryName }) => {
 	const [countryData, setCountryData] = useState({});
+	const [countryLocation, setCountryLocation] = useState({});
+	const [weatherData, setWeatherData] = useState();
 
 	useEffect(() => {
+		const api_key = import.meta.env.VITE_SOME_KEY;
+
 		axios
 			.get(
 				`https://studies.cs.helsinki.fi/restcountries/api/name/${countryName}`
@@ -19,6 +24,30 @@ const CountryInfo = ({ countryName }) => {
 					flagImg: response.data.flags.png,
 				};
 				setCountryData(data);
+
+				axios
+					.get(
+						`http://api.openweathermap.org/geo/1.0/direct?q=${data.capital}&limit=1&appid=${api_key}`
+					)
+					.then((response) => {
+						response.data.map((data) => {
+							const location = {
+								lat: data.lat,
+								lon: data.lon,
+							};
+							setCountryLocation(location);
+							axios
+								.get(
+									`https://api.openweathermap.org/data/2.5/weather?lat=${data.lat.toFixed(
+										2
+									)}&lon=${data.lon.toFixed(2)}&appid=${api_key}`
+								)
+								.then((response) => {
+									console.log(response.data);
+								});
+						});
+					})
+					.catch((error) => console.error(error));
 			})
 			.catch((error) => console.error("there was an error ", error));
 	}, [countryName]);
@@ -37,7 +66,12 @@ const CountryInfo = ({ countryName }) => {
 								<li key={language}>{language}</li>
 							))}
 					</ul>
-					<img src={countryData.flagImg && countryData.flagImg} alt="coountry flag" />
+					<img
+						src={countryData.flagImg && countryData.flagImg}
+						alt="country flag"
+					/>
+					<h2>Weather in {countryData.capital}</h2>
+					<p></p>
 				</>
 			) : (
 				<></>
