e922cb555d73f1a0f20a4569d0cf3c0f6ff1f34f
Nelman25 <jonelvillaver735@gmail.com>
2025-01-08 16:12:49 +0800

    added type bmi catergories + refactor

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -3,21 +3,30 @@ interface PersonInfo {
   weight: number;
 }
 
+type BMICatergories =
+  | "Underweight"
+  | "Normal weight"
+  | "Overweight"
+  | "Obesity";
+
 const parseArgument = (args: string[]): PersonInfo => {
   if (args.length < 4) throw new Error("Not enough arguments!");
   if (args.length > 4) throw new Error("Too many arguments!");
 
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3]),
-    };
-  } else {
-    throw new Error("Provided values are not numbers, please try again.");
+  const height = Number(args[2]);
+  const weight = Number(args[3]);
+
+  if (height <= 0) throw new Error("height must be greater than 0.");
+  if (weight <= 0) throw new Error("weight must be greater than 0.");
+
+  if (!isNaN(height) && !isNaN(weight)) {
+    return { height, weight };
   }
+
+  throw new Error("Provided values are not numbers, please try again.");
 };
 
-const calculateBMI = (weight: number, height: number): string => {
+const calculateBMI = (weight: number, height: number): BMICatergories => {
   const heightInMeters = height / 100;
   const BMI = weight / Math.pow(heightInMeters, 2);
 
