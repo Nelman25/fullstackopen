67eb88c4ffe2dc45eda55ce3ef404d7449d39390
Nelman25 <jonelvillaver735@gmail.com>
2025-01-08 16:32:48 +0800

    move exercises to separate folder

--- part9/exercises/bmiCalculator.ts
+++ part9/exercises/bmiCalculator.ts
@@ -9,9 +9,10 @@ type BMICatergories =
   | "Overweight"
   | "Obesity";
 
-const parseArgument = (args: string[]): PersonInfo => {
+const parseArgument = (args: string[]) => {
   if (args.length < 4) throw new Error("Not enough arguments!");
   if (args.length > 4) throw new Error("Too many arguments!");
+
 
   const height = Number(args[2]);
   const weight = Number(args[3]);
---
+++ part9/exercises/exerciseCalculator.ts
@@ -0,0 +1,11 @@
+interface Output {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+
